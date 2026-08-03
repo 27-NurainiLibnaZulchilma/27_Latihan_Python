70f6688ef04bd5d4e66fa79b644626340fc2d2f8
27_Nur'aini Libna Zulchilma <ima.tkjsmeksa25@gmail.com>
2026-08-03 10:18:11 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# Meminta pengguna untuk memasukkan angka
+angka = int(input("Masukkan angka:  "))
+# Memeriksa apakah angka akan habis dibagi 2 atau tidak
+if angka % 2 == 0:
+  print(f"{angka} adalah bilangan genap")  
+else:
+    print(f"{angka} adalah bilangan ganjil")
+
+print("---------------------------------------------")
+    
+print("============================================")
+print("                   MISAL AJA                ")
+print("============================================")
+x =int(input("Masukkan angka 1: "))
+y =int(input("Masukkan angka 2: "))
+print("Hasil Penjumlahan :", x / y)
