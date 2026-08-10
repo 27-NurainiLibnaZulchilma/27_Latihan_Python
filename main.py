2ab2438aedf4282b5be93a97e671838724c8951c
27_Nur'aini Libna Zulchilma <ima.tkjsmeksa25@gmail.com>
2026-08-10 11:08:37 +0700

    v2 main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# Meminta pengguna untuk memasukkan angka
-angka = int(input("Masukkan angka:  "))
-# Memeriksa apakah angka akan habis dibagi 2 atau tidak
-if angka % 2 == 0:
-  print(f"{angka} adalah bilangan genap")  
-else:
-    print(f"{angka} adalah bilangan ganjil")
-
-print("---------------------------------------------")
+# Perulangan 'for' agar program tidak close
+while True:
+    print("\n--- CEK BILANGAN GANJIL / GENAP ---")
     
-print("============================================")
-print("                   MISAL AJA                ")
-print("============================================")
-x =int(input("Masukkan angka 1: "))
-y =int(input("Masukkan angka 2: "))
-print("Hasil Penjumlahan :", x / y)
+    # Meminta pengguna untuk memasukkan angka
+    angka = int(input("Masukkan angka: "))
+    
+    # Memeriksa apakah angka habis dibagi 2 atau tidak
+    if angka % 2 == 0:
+        print(f"{angka} adalah bilangan genap")  
+    else:
+        print(f"{angka} adalah bilangan ganjil")
